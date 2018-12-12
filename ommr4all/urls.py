10ea8d0fdeb5a2cc0dd2ce3b8e38229c0a15abe3
C. Wick <wick.chr.info@gmail.com>
2018-12-12 23:03:24 +0100

    Added JWT authentication

--- ommr4all/urls.py
+++ ommr4all/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token-auth/', obtain_jwt_token),
+    path('api/token-refresh/', refresh_jwt_token),
+    path('api/token-verify/', verify_jwt_token),
     path('', include('main.urls')),
 ]
