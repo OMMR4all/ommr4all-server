75df034acf4f2999ee2211e8dae50027fbf1715f
Christoph Wick <wick.chr.info@gmail.com>
2018-12-17 16:46:49 +0100

    serving assets as static files, loading page

--- ommr4all/urls.py
+++ ommr4all/urls.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from django.conf.urls.static import static
 
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +13,6 @@ urlpatterns = [
     path('api/token-refresh/', refresh_jwt_token),
     path('api/token-verify/', verify_jwt_token),
     path('', include('main.urls')),
-]
+] \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static('assets', document_root=os.path.join(settings.STATIC_ROOT, 'ommr4all-client', 'assets'))
