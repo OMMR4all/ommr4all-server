b3067a8aac2de5b04aa489f35638db487c9fae65
Christoph Wick <wick.chr.info@gmail.com>
2019-08-12 11:08:06 +0200

    Fixed static icon serve (assets including i18n), removed lint to material font

--- ommr4all/urls.py
+++ ommr4all/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
 
-from django.conf.urls.static import static
+from django.conf.urls.static import serve, static
 
 import os
 
@@ -13,7 +13,7 @@ urlpatterns = \
         path('api/', include('restapi.urls')),
     ] \
     + i18n_patterns(
-        *static('assets', document_root=os.path.join(settings.STATIC_ROOT, 'ommr4all-client', 'assets')),
+        re_path(r'^assets(?P<path>.*)$', serve, {'document_root': os.path.join(settings.STATIC_ROOT, 'ommr4all-client', 'assets')}),
         path('', include('webapp.urls')),
         ) \
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
