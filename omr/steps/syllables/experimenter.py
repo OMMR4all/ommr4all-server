f9e79beb068b4b100f52b4f8553cb140092603fc
C. Wick <wick.chr.info@gmail.com>
2019-11-08 11:52:57 +0100

    fixed counting of errors in the syllable detection

--- omr/steps/syllables/experimenter.py
+++ omr/steps/syllables/experimenter.py
@@ -2,7 +2,7 @@ from typing import List
 
 from database import DatabaseBook
 from database.file_formats import PcGts
-from database.file_formats.pcgts.page import Annotations
+from database.file_formats.pcgts.page import Annotations, GraphicalConnectionType
 from omr.experimenter.experimenter import Experimenter
 from .predictor import PredictionResult
 from prettytable import PrettyTable
@@ -31,8 +31,14 @@ class SyllablesExperimenter(Experimenter):
         def flatten(x):
             return sum(sum(x, []), [])
 
-        pred = [[[(sc.syllable.id, sc.note.id) for sc in c.syllable_connections] for c in p.annotations.connections] for p in full_predictions]
-        gt = [[[(sc.syllable.id, sc.note.id) for sc in c.syllable_connections] for c in p.page().annotations.connections] for p in full_predictions]
+        pred, gt = [], []
+        for p in full_predictions:
+            notes = {
+                c.music_region.id: [s for s in c.music_region.lines[0].symbols if s.graphical_connection == GraphicalConnectionType.NEUME_START] for c in p.annotations.connections
+            }
+            pred.append([[(p.page().location.page, c.music_region.id, c.text_region.id, sc.syllable.id, notes[c.music_region.id].index(sc.note)) for sc in c.syllable_connections] for c in p.annotations.connections])
+            gt.append([[(p.page().location.page, c.music_region.id, c.text_region.id, sc.syllable.id, notes[c.music_region.id].index(sc.note)) for sc in c.syllable_connections] for c in p.page().annotations.connections])
+
         return flatten(gt), flatten(pred)
 
     def output_prediction_to_book(self, pred_book: DatabaseBook, output_pcgts: List[PcGts], predictions: List[PredictionResult]):
@@ -40,10 +46,39 @@ class SyllablesExperimenter(Experimenter):
             pcgts.page.annotations = Annotations.from_json(pred.annotations.to_json(), pcgts.page)
 
     def evaluate(self, predictions, evaluation_params):
+        render = False
+
         gt, pred = predictions
-        tp = len([x for x in gt if x in pred])
-        fp = len([x for x in pred if x not in gt])
-        fn = len([x for x in gt if x not in pred])
+
+        # compute distances
+        tps = [x for x in gt if x in pred]
+        fps = [x for x in pred if x not in gt]
+        fns = [x for x in gt if x not in pred]
+
+        distances = []
+        for e in pred:
+            matching_s = [g for g in gt if g[:4] == e[:4]][0]
+            d = e[-1] - matching_s[-1]
+            distances.append(d)
+
+        distances = np.array(distances)
+        if render:
+            import matplotlib.pyplot as plt
+            labels, counts = np.unique(distances, return_counts=True)
+            r = np.arange(distances.min(), distances.max() + 1)
+            counts = [(counts[np.where(labels == i)] if i in labels else 0) for i in r]
+            plt.bar(r, counts, align='center')
+            # plt.gca().set_yscale('log')
+            plt.gca().set_xticks(r)
+            plt.grid(axis='y')
+            plt.gca().set_xlabel('Distance to correct neume')
+            plt.gca().set_ylabel('Count')
+            plt.show()
+
+        # compute f1 scores
+        tp = len(tps)
+        fp = len(fps)
+        fn = len(fns)
 
         p = tp / (tp + fp)
         r = tp / (tp + fn)
