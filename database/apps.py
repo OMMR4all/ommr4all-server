a8b2be139379f10f0d01f3ca294526423dc32b52
Alexander Hartelt <hartelt.alexander@gmail.com>
2022-10-19 17:34:25 +0200

    torch multiprocessing workaround

--- database/apps.py
+++ database/apps.py
@@ -1,5 +1,12 @@
+import torch
 from django.apps import AppConfig
+try:
+    # torch fork problem workaround
+    torch.set_num_threads(1)
+except RuntimeError:
+    pass
 
 
 class DatabaseConfig(AppConfig):
     name = 'database'
+
