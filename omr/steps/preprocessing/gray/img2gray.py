959d20a8d0ec71e73f298ba7c6ef995af245b1f7
Alexander Hartelt <hartelt.alexander@gmail.com>
2023-03-31 14:26:52 +0200

    Improved simple layout recognition

--- omr/steps/preprocessing/gray/img2gray.py
+++ omr/steps/preprocessing/gray/img2gray.py
@@ -4,7 +4,7 @@ import logging
 
 
 def im2gray(img: Image) -> Image:
-    ni = np.array(img).astype(np.float)
+    ni = np.array(img).astype(float)
     if ni.ndim == 3:
         return Image.fromarray(((ni[:, :, 2] + ni[:, :, 1] + ni[:, :, 0]) / 3).astype(np.uint8))
 
